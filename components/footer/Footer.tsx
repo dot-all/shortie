8413eb53d50b625e4d539b93983a2e0aa1b9fd96
im-allan <al.silvaquiroz@gmail.com>
2024-04-04 22:07:09 -0300

    feat: Added footer section

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="h-64 w-11/12 s:w-10/12 mx-auto relative max-w-7xl">
+      <hr className="w-full absolute bottom-20"/>
+      <p className="absolute bottom-4 text-sm sm:text-base">Hecho por <a href="" className="text-[#121180]/50 font-bold">@im-allan</a></p>
+    </footer>
+  )
+}
