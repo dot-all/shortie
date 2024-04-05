cdb07181a444c90c06a94dee50eaa693c7121f41
im-allan <al.silvaquiroz@gmail.com>
2024-04-04 22:10:29 -0300

    feat: new prod version

--- app/components/FooterSection.tsx
+++
@@ -1,9 +0,0 @@
-export default function FooterSection() {
-  return (
-    <footer className="h-64 w-11/12 s:w-10/12 mx-auto relative max-w-7xl">
-      <h2 className="text-2xl font-bold mt-12">SHORTIE</h2>
-      <hr className="w-full absolute bottom-20"/>
-      <p className="absolute bottom-4 text-sm s:text-base">Hecho por <a href="" className="text-[#1864FC]/50 font-bold">@im-allan</a></p>
-    </footer>
-  )
-}
