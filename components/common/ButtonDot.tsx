1372d51c35f67525de8a4364acc6933a3ff30696
im-allan <al.silvaquiroz@gmail.com>
2024-04-08 18:42:43 -0400

    feat: resize button dot component

--- components/common/ButtonDot.tsx
+++ components/common/ButtonDot.tsx
@@ -5,7 +5,7 @@ interface ButtonDotProps {
 export default function ButtonDot({def}: ButtonDotProps) {
   return (
     <button className="b absolute top-2 right-2" onClick={def}>
-      <span className="relative py-3 px-4 flex justify-center items-center ">
+      <span className="relative py-3 px-3 flex justify-center items-center ">
         <span className="i h-full w-full bg-[#9D00A3] items-center rounded-lg shadow-2xl cursor-pointer absolute overflow-hidden transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out">
         </span>
         <span className="absolute flex h-4 w-4 top-1 right-1 transform translate-x-2.5 -translate-y-2.5">
