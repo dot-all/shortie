10990468a4f2b2b2f0f7a628385d26705d3446a2
dot-all <al.quirozsilva@gmail.com>
2024-06-18 19:18:31 -0400

    feat: added new href

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="h-64 w-11/12 s:w-10/12 mx-auto relative max-w-7xl">
       <hr className="w-full absolute bottom-20"/>
-      <p className="absolute bottom-4 text-sm sm:text-base">Hecho por <a href="" className="text-[#121180]/50 font-bold">@im-allan</a></p>
+      <p className="absolute bottom-4 text-sm sm:text-base">Hecho por <a href="https://github.com/dot-all" target="_blank" className="text-[#121180]/50 font-bold">@dot-all</a></p>
     </footer>
   )
 }
